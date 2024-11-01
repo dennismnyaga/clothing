28047cdc3d92548cc77d2501ae610771fc9d3c6b
dennis nyaga <murildennis7@gmail.com>
2024-11-02 00:51:40 +0300

    added the more funcs to the advance

--- src/pages/Customers.tsx
+++ src/pages/Customers.tsx
@@ -3,6 +3,7 @@ import { useAppDispatch, useAppSelector } from '../app/hooks';
 import { toggleSidebar } from '../features/sidebar/sidebarSlice';
 import LeftNav from '../components/LeftNav';
 import TopNavBar from '../components/TopNavBar';
+import constr from '../assets/images/under-construction.png'
 
 const Customers = () => {
     const dispatch = useAppDispatch();
@@ -17,7 +18,7 @@ const Customers = () => {
             <div className="flex-1 bg-gray-100">
                 <TopNavBar isCollapsed={isCollapsed} toggleSidebar={handleToggleSidebar} />
                 <div className="p-6 max-h-[550px] overflow-y-auto">
-                    <h1>Customers</h1>
+                <img src={constr} alt='coming-soon' loading='lazy'/>
                 </div>
             </div>
         </div>
